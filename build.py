ae7a955c1be7cf5e1f83d50f24bb0ff57bd5ce82
Scott Hilbert <github@shilbert.com>
2012-09-24 17:18:52 -0400

    REFACTOR

--- build.py
+++ build.py
@@ -7,9 +7,14 @@ import gzip
 import yaml
 from datetime import datetime
 
+def pretty_date(when):
+	month = when.strftime('%B ')
+	day_year = when.strftime('%d, %Y').lstrip('0')
+	return month+day_year
+
 class Post(object):
 	def __init__(self, src_path):
-		basename = os.path.basename(post_path)
+		basename = os.path.basename(src_path)
 		non_ext, ext = os.path.splitext(basename)
 
 		src_text = codecs.open(src_path, "r", "utf-8").read().strip()
@@ -29,48 +34,48 @@ class Post(object):
 	def contents_html(self):
 		return markdown2.markdown(self.contents_markdown, extras=['smarty-pants'])
 
-def render_to_file(template_name, output_path, template_args):
-	template = env.get_template(template_name)
-	final_html_utf8 = template.render(template_args).encode('utf-8')
-	with open(os.path.join(OUTPUT_DIR, output_path), 'wb') as f:
-		f.write(final_html_utf8)
-	if CONFIG.get('gzip'):
-		with gzip.open(os.path.join(OUTPUT_DIR, output_path+'gz'), 'wb') as f:
-			f.write(final_html_utf8)
+class Builder(object):
+	def __init__(self, config_path):
+		self.config = yaml.load(codecs.open('config.yml', 'r', 'utf-8'))
+		
+	@property
+	def output_dir(self):
+		return self.config['dirs']['output']
+		
+	def build(self):
+		self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(self.config['dirs']['templates']),
+									  autoescape=True)
+		self.env.filters['pretty_date'] = pretty_date
+		
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir)
+		else:
+			for f in os.listdir(self.output_dir):
+				f_path = os.path.join(self.output_dir, f)
+				if os.path.isfile(f_path):
+					os.unlink(f_path)
 
-def pretty_date(when):
-	month = when.strftime('%B')
-	day = when.strftime('%d').lstrip('0')
-	year = when.strftime('%Y')
-	return "%s %s, %s" % (month,day,year)
+		posts_dir = self.config['dirs']['posts']
+		posts = []
+		for f in os.listdir(posts_dir):
+			post_path = os.path.join(posts_dir, f)
+			if os.path.isfile(post_path) and f.endswith('.md'):
+				posts.append(Post(post_path))
+
+		for post in posts:
+			self.render_to_file(self.config['templates']['post'], post.name+'.html', {
+				'site': self.config.get('site'),
+				'post':post
+			})
+		
+	def render_to_file(self, template_name, output_path, template_args):
+		template = self.env.get_template(template_name)
+		final_html_utf8 = template.render(template_args).encode('utf-8')
+		with open(os.path.join(self.output_dir, output_path), 'wb') as f:
+			f.write(final_html_utf8)
+		if self.config.get('gzip'):
+			with gzip.open(os.path.join(self.output_dir, output_path+'gz'), 'wb') as f:
+				f.write(final_html_utf8)
 
 if __name__ == '__main__':
-	global CONFIG, OUTPUT_DIR, env
-	CONFIG = yaml.load(codecs.open('config.yml', 'r', 'utf-8'))
-	#print CONFIG
-	OUTPUT_DIR = CONFIG['dirs']['output']
-	POSTS_DIR = CONFIG['dirs']['posts']
-
-	env = jinja2.Environment(loader=jinja2.FileSystemLoader(CONFIG['dirs']['templates']),
-							 autoescape=True)
-	env.filters['pretty_date'] = pretty_date
-
-	if not os.path.exists(OUTPUT_DIR):
-		os.makedirs(OUTPUT_DIR)
-	else:
-		for f in os.listdir(OUTPUT_DIR):
-			f_path = os.path.join(OUTPUT_DIR, f)
-			if os.path.isfile(f_path):
-				os.unlink(f_path)
-
-	posts = []
-	for f in os.listdir(POSTS_DIR):
-		post_path = os.path.join(POSTS_DIR, f)
-		if os.path.isfile(post_path) and f.endswith('.md'):
-			posts.append(Post(post_path))
-
-	for post in posts:
-		render_to_file(CONFIG['templates']['post'], post.name+'.html', {
-			'site': CONFIG.get('site'),
-			'post':post
-		})
+	Builder('config.yml').build()
