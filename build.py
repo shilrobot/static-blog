80396c250123a0789fac2e24ec56bf0e4baa1c62
Scott Hilbert <github@shilbert.com>
2012-09-24 20:59:47 -0400

    Working on getting it linked up into a real site...

--- build.py
+++ build.py
@@ -32,13 +32,27 @@ class Post(object):
 		self.title = yaml_header.get('title','')
 		self.contents_markdown = src_text[split_idx+len(SPLITTER):]
 		self.contents_html = markdown2.markdown(self.contents_markdown, extras=self.builder.config.get('markdown_extras'))
-
+		self.older = None
+		self.newer = None
+	
+	@property
+	def uri(self):
+		return self.name + '.html'
+		
 class PostGroup(object):
-	def __init__(self, index, posts):
+	def __init__(self, builder, index, posts):
+		self.builder = builder
 		self.index = index
 		self.posts = posts[:]
 		self.newer = None
 		self.older = None
+		
+	@property
+	def uri(self):
+		if self.index == 0:
+			return 'index.html'
+		else:
+			return '%d.html' % (self.index+1)
 		
 class Builder(object):
 	def __init__(self, config_path):
@@ -69,12 +83,16 @@ class Builder(object):
 				posts.append(Post(self, post_path))
 				
 		posts = sorted(posts, key=lambda p:p.date, reverse=True)
+		assert len(posts) > 0
 
-		for post in posts:
-			self.render_to_file(self.config['templates']['post'], post.name+'.html', {
-				'post':post
-			})
-			
+		# link up previous/next references
+		for (n,post) in enumerate(posts):
+			post.index = len(posts)-n-1
+			if n > 0:
+				post.newer = posts[n-1]
+			if n < (len(posts)-1):
+				post.older = posts[n+1]				
+		
 		# group posts into several per page
 		curr_list = []
 		curr_pg_idx = 0
@@ -83,11 +101,11 @@ class Builder(object):
 		for post in posts:
 			curr_list.append(post)
 			if len(curr_list) >= posts_per_group:
-				post_groups.append(PostGroup(curr_pg_idx, curr_list))
+				post_groups.append(PostGroup(self, curr_pg_idx, curr_list))
 				curr_list = []
 				curr_pg_idx += 1
 		if len(curr_list) > 0:
-			post_groups.append(PostGroup(curr_pg_idx, curr_list))
+			post_groups.append(PostGroup(self, curr_pg_idx, curr_list))
 		
 		# link up previous/next references
 		for post_group in post_groups:
@@ -96,18 +114,26 @@ class Builder(object):
 			if post_group.index < (len(post_groups)-1):
 				post_group.older = post_groups[post_group.index+1]
 		
+		self.posts = posts
+		self.post_groups = post_groups
+		
+		for post in posts:
+			self.render_to_file(self.config['templates']['post'], post.uri, {
+				'post':post
+			})
+			
 		for post_group in post_groups:
-			if post_group.index == 0:
-				output_name = 'index.html'
-			else:
-				output_name = '%d.html' % (post_group.index+1)
-			self.render_to_file(self.config['templates']['post_group'], output_name, {
-				'post_group':post_group
+			self.render_to_file(self.config['templates']['post_group'], post_group.uri, {
+				'post_group':post_group,
 			})
 		
 	def render_to_file(self, template_name, output_path, template_args):
 		template_args = template_args.copy()
-		template_args['site'] = self.config.get('site')
+		template_args.update({
+			'site': self.config.get('site'),
+			'posts': self.posts,
+			'post_groups': self.post_groups
+		})
 		template = self.env.get_template(template_name)
 		final_html_utf8 = template.render(template_args).encode('utf-8')
 		with open(os.path.join(self.output_dir, output_path), 'wb') as f:
